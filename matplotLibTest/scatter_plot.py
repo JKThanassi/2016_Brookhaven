316b043d4bef6152ef5cff7baeb3a782d88c405a
JKThanassi <josephkamingthanassi@gmail.com>
2016-07-12 10:27:22 -0400

    scatter plot script done

---
+++ matplotLibTest/scatter_plot.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+def scatter_plt():
+
+    #list init
+    x = []
+    y = []
+
+    #get list length
+    list_length = int(input("How many data sets would you like to enter"))
+
+    #get input
+    for i in range(0, list_length):
+        x.append(int(input("enter x var for dataset " + str(i))))
+        y.append(int(input("enter y var for dataset " + str(i))))
+        print()
+
+    #create scatterplot
+    plt.scatter(x, y, marker='*')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Test scatter plt")
+    plt.show()
+
+
+scatter_plt()
